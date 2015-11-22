be1d19eef0993e49672b8b9843f92d35a4a13912
Kaja Coraor <kaja.coraor@gmail.com>
2015-11-22 16:22:53 -0500

    Added templates for getting started, FAQs

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^getstarted/$', TemplateView.as_view(template_name='getting_started.html'), name='getstarted'),
+    url(r'^faq/$', TemplateView.as_view(template_name='faq.html'), name='faq'),
+    url(r'^signup/$', TemplateView.as_view(template_name='signup.html'), name='signup'),
     url(r'^room/$', views.DetailRoomList.as_view(), name='detail_list'),
     url(r'^room/(?P<pid>[0-9]+)/$', views.room_detail, name='detail'),
     url(r'^room/thanks/$', TemplateView.as_view(template_name='thanks.html'), name='thanks'),
