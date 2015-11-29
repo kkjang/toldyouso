67936ff5f2bfd67031710985fabdb5739b33416a
plungg <pl@live.unc.edu>
2015-11-29 15:57:25 -0500

    trying to get url params to work

--- static_comp/site_specific/js/myapp.js
+++ static_comp/site_specific/js/myapp.js
@@ -4,7 +4,7 @@ var my_app = angular.module('myapp', ['ng.django.urls', /* dependencies */]).con
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 });
 
-my_app.controller('RoomController', function($scope, $http, $window, djangoUrl) {
+my_app.controller('RoomController', function($scope, $http, $window, $location, djangoUrl) {
 
     $scope.submit = function() {
         var in_data = angular.toJson($scope.room_data);
@@ -21,6 +21,22 @@ my_app.controller('RoomController', function($scope, $http, $window, djangoUrl) 
         $http.get(djangoUrl.reverse('room-list'))
             .success(function (data){
                 console.log(data);
+                $scope.allRooms = data.results; 
+                console.log('$scope.allRooms = ', $scope.allRooms);
+            })
+    }
+
+    $scope.getKeyFromUrl = function($location){
+            console.log($location);
+            console.log($location.search().q);
+            $http({ //toldyouso.com/room/rooms/1?title=abc
+                url: user.details_path, //toldyouso.com/room/rooms/1
+                method: "GET",
+                params: {title: $location.search()} //abc
+             })
+            .success(function (data){
+                console.log(data);
+                console.log(djangoUrl.reverse('room-list'));
                 $scope.allRooms = data.results; 
                 console.log('$scope.allRooms = ', $scope.allRooms);
             })
