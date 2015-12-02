78794fe5d06c6286629260f540c96a4b4446c75c
kkjang <kevinjang7@gmail.com>
2015-12-01 20:14:36 -0500

    New Model In!!! Only for creation.

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 router = routers.SimpleRouter()
 router.register(r'rooms', views.RoomSetView)
+router.register(r'wagers', views.WagerSetView)
+router.register(r'bets', views.BetSetView)
+
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
@@ -16,7 +19,7 @@ urlpatterns = [
     url(r'^room/$', views.DetailRoomList.as_view(), name='detail_list'),
     url(r'^room/(?P<pid>[0-9]+)/$', views.room_detail, name='detail'),
     url(r'^room/thanks/$', TemplateView.as_view(template_name='thanks.html'), name='thanks'),
-    url(r'^room/submit/$', views.SubmitRoomFormView.as_view(), name='submit'),
+    url(r'^room/submit/$', views.SubmitBetFormView.as_view(), name='submit'),
     url(r'^room/find/$', views.find_room_from_key, name='find'),
     url(r'^room/edit/(?P<room_key>[^/]+)/$', views.submit_challenged, name='edit'),
     url(r'^room/error/$', TemplateView.as_view(template_name='room_ready_error.html'), name='room_ready_error'),
