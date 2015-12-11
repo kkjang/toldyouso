8267226554ae345dc220e31013410338a52d7e9b
kkjang <kevinjang7@gmail.com>
2015-12-11 15:10:51 -0500

    userregisterform

--- myapp/views.py
+++ myapp/views.py
@@ -16,7 +16,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from .serializers import BetSerializer, WagerSerializer
-from .forms import UserLoginForm, SubmitWagerForm, SubmitBetForm
+from .forms import UserLoginForm, SubmitWagerForm, SubmitBetForm, UserRegisterForm
 from rest_framework.exceptions import PermissionDenied
 
 
